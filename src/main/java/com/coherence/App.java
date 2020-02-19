4e4749dd8178f8ca4e38b156bef1d68fb51007df
sudeeptamajhee <sudeeptamajhee@yahoo.com>
2020-02-19 11:47:59 +0530

    Coherence Cache

---
+++ src/main/java/com/coherence/App.java
@@ -0,0 +1,24 @@
+package com.coherence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
+
+public class App {
+	public static void main(String[] args) {
+		CacheFactory.ensureCluster();
+		NamedCache cache = CacheFactory.getCache("hello-example");
+
+		cache.put("k1", "Hello World!");
+		cache.put("k2", "Hello India!");
+		
+		List<String> keys = new ArrayList<String>();
+		keys.add("k1");
+		keys.add("k2");
+		System.out.println(cache.getAll(keys));
+
+		CacheFactory.shutdown();
+	}
+}
---
+++ verify-coherence-cache.cmd
@@ -0,0 +1,1 @@
+java -cp C:/dev/other/coherence-cache/src/main/resources;C:/Users/538040/.m2/repository/com/oracle/coherence/coherence/3.7.1/coherence-3.7.1.jar com.tangosol.net.CacheFactory
